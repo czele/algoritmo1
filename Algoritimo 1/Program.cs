270c61a6af6ada3b32cb18a497f36544fbddc905
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-10 23:35:09 -0300

    Algoritmo1

---
+++ Algoritimo 1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+
+namespace Algoritimo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCÍCIO 3 - FORCA - não entendi
+
+            int maxTentativas = 7;
+            string palavra = "DESENVOLVEDOR";
+            int qcaracteres = palavra.Length;
+            string segredo = "";
+
+            for (int i = 0; i < qcaracteres; i++)
+            {
+                segredo = segredo + "_ "; 
+            }
+
+            string letra = "";
+
+            for (int i = 0; i < maxTentativas; i++)
+            {
+                Console.WriteLine(segredo);
+                Console.WriteLine("Informe uma letra");
+                letra = Console.ReadLine();
+                letra = letra.ToUpper();
+
+                if (letra.Length == 1)
+
+                    int qntdeEspaços = 0;
+                {
+                    for (int l = 0; l < qcaracteres; l++)
+                    {
+                        string letter = palavra.Substring(l, 1);
+                        if (1 != 0)
+                            qtdeEspacos++;
+
+                        if (letter == letra)
+                        {
+                            int indexSubString = l; 
+
+                            if (l == 0)
+                                indexSubString = l;
+                            else
+                                indexSubString = l + qtdeEspaços + 1;
+
+                            StringBuilder sb = new StringBuilder(segredo);
+                            sb[indexSubString] = Convert.ToChar(letter);
+                            segredo = sb.ToString();
+                        }
+
+
+                            
+
+                        
+                    }
+                }
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            //EXERCÍCIO 2 - Tempo de Viagem
+
+            //Console.WriteLine("Digite a distância (km)");            
+            //double distancia = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Digite a velocidade (km/h)");
+            //double velocidade = Convert.ToDouble(Console.ReadLine());
+
+            //double tempo = distancia / velocidade;
+            //Console.WriteLine($"Você chegará ao seu destino em {tempo} horas.");
+
+
+            //EXERCÍCIO 1 - Nota do Aluno
+            //Console.WriteLine("Digite a primeira nota: ");
+            //double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Digite a segunda nota: ");
+            //double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Digite a terceira nota: ");
+            //double nota3 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Digite a quarta nota: ");
+            //double nota4 = Convert.ToDouble(Console.ReadLine());
+
+            //double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            //Console.WriteLine(media);
+
+            //if (media <= 6.9)
+            //    {
+            //    Console.WriteLine($"A média do aluno foi: (media)");
+            //    Console.WriteLine(" - Aluno reprovado");
+            //}
+
+            //if (media >= 7)
+            //{
+            //    Console.WriteLine($"A média do aluno foi: {media}");
+            //    Console.WriteLine(" - Aluno aprovado");
+            //}
+        }
+    }
+}
