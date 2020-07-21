cb6b35437d00343f8b142dde0a6e87bfa31106ca
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-21 02:28:14 -0300

    adicionar gitignore

--- Algoritimo 1/Program.cs
+++ Algoritimo 1/Program.cs
@@ -7,70 +7,70 @@ namespace Algoritimo_1
     {
         static void Main(string[] args)
         {
-            //EXERCÍCIO 3 - FORCA - não entendi
+            //            EXERCÍCIO 3 - FORCA - não entendi
 
-            int maxTentativas = 7;
-            string palavra = "DESENVOLVEDOR";
-            int qcaracteres = palavra.Length;
-            string segredo = "";
+            //int maxTentativas = 7;
+            //            string palavra = "DESENVOLVEDOR";
+            //            int qcaracteres = palavra.Length;
+            //            string segredo = "";
 
-            for (int i = 0; i < qcaracteres; i++)
-            {
-                segredo = segredo + "_ "; 
-            }
+            //            for (int i = 0; i < qcaracteres; i++)
+            //            {
+            //                segredo = segredo + "_ ";
+            //            }
 
-            string letra = "";
+            //            string letra = "";
 
-            for (int i = 0; i < maxTentativas; i++)
-            {
-                Console.WriteLine(segredo);
-                Console.WriteLine("Informe uma letra");
-                letra = Console.ReadLine();
-                letra = letra.ToUpper();
+            //            for (int i = 0; i < maxTentativas; i++)
+            //            {
+            //                Console.WriteLine(segredo);
+            //                Console.WriteLine("Informe uma letra");
+            //                letra = Console.ReadLine();
+            //                letra = letra.ToUpper();
 
-                if (letra.Length == 1)
+            //                if (letra.Length == 1)
 
-                    int qntdeEspaços = 0;
-                {
-                    for (int l = 0; l < qcaracteres; l++)
-                    {
-                        string letter = palavra.Substring(l, 1);
-                        if (1 != 0)
-                            qtdeEspacos++;
+            //                    int qntdeEspaços = 0;
+            //                {
+            //                    for (int l = 0; l < qcaracteres; l++)
+            //                    {
+            //                        string letter = palavra.Substring(l, 1);
+            //                        if (1 != 0)
+            //                            qtdeEspacos++;
 
-                        if (letter == letra)
-                        {
-                            int indexSubString = l; 
+            //                        if (letter == letra)
+            //                        {
+            //                            int indexSubString = l;
 
-                            if (l == 0)
-                                indexSubString = l;
-                            else
-                                indexSubString = l + qtdeEspaços + 1;
+            //                            if (l == 0)
+            //                                indexSubString = l;
+            //                            else
+            //                                indexSubString = l + qtdeEspaços + 1;
 
-                            StringBuilder sb = new StringBuilder(segredo);
-                            sb[indexSubString] = Convert.ToChar(letter);
-                            segredo = sb.ToString();
-                        }
+            //                            StringBuilder sb = new StringBuilder(segredo);
+            //                            sb[indexSubString] = Convert.ToChar(letter);
+            //                            segredo = sb.ToString();
+            //                        }
 
 
-                            
 
-                        
-                    }
-                }
-                
-            }
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+            //                    }
+            //                }
+
+            //            }
+
+
+
+
+
+
+
+
             //EXERCÍCIO 2 - Tempo de Viagem
 
-            //Console.WriteLine("Digite a distância (km)");            
+            //Console.WriteLine("Digite a distância (km)");
             //double distancia = Convert.ToDouble(Console.ReadLine());
 
             //Console.WriteLine("Digite a velocidade (km/h)");
@@ -98,7 +98,7 @@ namespace Algoritimo_1
             //Console.WriteLine(media);
 
             //if (media <= 6.9)
-            //    {
+            //{
             //    Console.WriteLine($"A média do aluno foi: (media)");
             //    Console.WriteLine(" - Aluno reprovado");
             //}
